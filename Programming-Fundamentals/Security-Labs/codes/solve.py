beaae66887002344f95259af66efcbbabfdb812c
Tiger1218 <tiger1218@foxmail.com>
2022-12-31 20:57:07 +0800

    add pdf version report

---
+++ Programming-Fundamentals/Security-Labs/codes/solve.py
@@ -0,0 +1,23 @@
+data = [
+	0x63636363, 0x63636363, 0x72464663, 0x6F6D6F72,
+      	0x466D203A, 0x65693A72, 0x43646E20, 0x6F54540A,
+      	0x5920453A, 0x54756F0A, 0x6F6F470A, 0x21643A6F,
+      	0x594E2020, 0x206F776F, 0x79727574, 0x4563200A,
+      	0x6F786F68, 0x6E696373, 0x6C206765, 0x796C656B,
+      	0x2C336573, 0x7420346E, 0x20216F74, 0x726F5966,
+      	0x7565636F, 0x20206120, 0x6C616763, 0x74206C6F,
+      	0x20206F74, 0x74786565, 0x65617276, 0x32727463,
+      	0x6E617920, 0x680A6474, 0x6F697661, 0x20646E69,
+      	0x21687467, 0x63002065, 0x6C6C7861, 0x78742078,
+      	0x6578206F, 0x72747878, 0x78636178, 0x00783174
+]
+
+real_data = []
+
+for num in data:
+    for _ in range(4):
+        real_data.append(num % 0x100)
+        num //= 0x100
+
+print("real_data = ", [hex(i)[2:].ljust(2, " ") for i in real_data])
+print(("real_string = '" + "".join([chr(i) for i in real_data]) + "'").replace("\n", r"\n"))
